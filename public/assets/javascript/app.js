6aab21d069efc894df4e5c017cf428fc3ac5b11c
Paul M Herman <>
2018-03-30 22:41:42 -0400

    clean up

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -1,37 +1,46 @@
+$(document).ready(function() {
+    function displayNote(e) {
+        e.preventDefault();
+        var thisId = $(this).attr("value");    
+        $.ajax({
+        method: "GET",
+        url: "/save/" + thisId
+        })
+        .then(function(data) {
+            if (data.note) {
+                $(".modalTitle").html("<h4>" + data.headline + "</h4>");
+                $("#noteTitle").val(data.note.title);
+                $("#noteBody").val(data.note.body);
+            }
+        });
 
-function displayNote(e) {
-    e.preventDefault();
-    var thisId = $(this).attr("value");
-  
-    $.ajax({
-      method: "GET",
-      url: "/save/" + thisId
-    })
-      .then(function(data) {
-        if (data.note) {
-            $(".modalTitle").html("<h4>" + data.headline + "</h4>");
-            $("#noteTitle").val(data.note.title);
-            $("#noteBody").val(data.note.body);
-            console.log("Title: " + data.headline);
-            console.log("ID: " + thisId);
-            console.log("noteTitle: " + data.note.title);
-        }
-      });
+    }
 
-}
-
-function newNote(e) {
-    e.preventDefault();
-    var id=$(this).attr("value");
-    console.log("id: " + id);
-    var note = {
-        title: $("#noteTitle").val().trim(),
-        body: $("#noteBody").val().trim()
+    function newNote(e) {
+        var thisId = $(".editNote").attr("value");
+        console.log("id: " + this.Id);
+        $.ajax({
+            method: "POST",
+            url: "/note/" + thisId,
+            data: {
+              title: $("#noteTitle").val(),
+              body: $("#noteBody").val()
+            }
+          })
+          .then(function(data) {
+          });
     };
-    $.post("/note/" + id, note, function(data) {
-    })
-}
-// Save Note button in modal
-$(document).on("click", "#addNote", newNote);
-//Edit Note button on saved page 
-$(document).on("click", ".editNote", displayNote);
+
+    function scrape(e) {
+        e.preventDefault();
+        location.reload();
+        console.log("page reloaded");
+    }
+
+    // Save Note button in modal
+    $(document).on("click", "#addNote", newNote);
+    //Edit Note button on saved page 
+    $(".col-lg-12").on("click", ".editNote", displayNote);
+    //Refreshes page when scrape is executed
+    $(document).on("click", "#scrapeDismiss", scrape);
+});
